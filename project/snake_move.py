d9e84186b1a0afe52ef6525b811639bba88bc78d
Cho_YongJoon <yzz1108j@gmail.com>
2022-10-13 16:54:35 +0900

    Project - apple position update
    2019142034

--- project/snake_move.py
+++ project/snake_move.py
@@ -1,6 +1,8 @@
+from gettext import find
 from pico2d import *
 from random import *
 from math import *
+from numpy import *
 
 UI_WIDTH, UI_HEIGHT = 920, 640
 open_canvas(UI_WIDTH, UI_HEIGHT)
@@ -19,12 +21,17 @@ snake_speed = 5
 dx = [snake_speed, 0, -snake_speed, 0, 0]
 dy = [0, +snake_speed, 0, -snake_speed, 0]
 
+def field_array_reset():
+    global field_array
+    field_array = [[16] * 11]
+    for i in range(0, 15):
+        field_array += [[16] + [0] * 9 + [16]]
+    field_array += [[16] * 11]
+
 field_dict = {'empty': 0, 'player': 1, 'enemy':2, 'apple':4, \
     'bomb':8, 'wall':16, 'head':32}
-field_array = [[16] * 11] #0:empty, 1:player, 2:enemy, 4:apple, 8:bomb, 16:wall, 32:head
-for i in range(0, 15):
-    field_array += [[16] + [0] * 9 + [16]]
-field_array += [[16] * 11]
+field_array = [] #0:empty, 1:player, 2:enemy, 4:apple, 8:bomb, 16:wall, 32:head
+field_array_reset()
 
 def convert_coordinates(x, y):
     return x, UI_HEIGHT - y
@@ -69,12 +76,13 @@ class blue_body():
 class apple():
     def __init__(self, gx, gy):
         self.x, self.y = grid_to_coordinates(gx, gy)
+        self.gx, self.gy = gx, gy
         self.image = img_apple
         self.exist = True
-        field_array[gx+1][gy+1] |= field_dict['apple']
     def draw(self):
         if(self.exist):
             self.image.draw(self.x, self.y)
+            field_array[self.gx+1][self.gy+1] |= field_dict['apple']
         else: return
 
 length = 12*(3-1)+1
@@ -89,13 +97,13 @@ def handle_events():
             acting = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             acting = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_w and cur_direction not in (1,3,4):
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_w and cur_direction not in (1,3):
             direction = 1
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_a and cur_direction not in (0,2,4):
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_a and cur_direction not in (0,2):
             direction = 2
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_s and cur_direction not in (1,3,4):
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_s and cur_direction not in (1,3):
             direction = 3
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_d and cur_direction not in (0,2,4):
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_d and cur_direction not in (0,2):
             direction = 0
 
 def snake_move_and_draw():
@@ -115,7 +123,20 @@ def check_eat():
                 char_blue.append(blue_body(i, char_blue[length-1].x, char_blue[length-1].y))
             length += 12
         del(appl)
-        appl = apple(randint(0, 14), randint(0, 8))
+        new_apple_x = 0
+        new_apple_y = 0
+        while(field_array[new_apple_x][new_apple_y] != 0 \
+            or field_array[new_apple_x][new_apple_y-1] != 0 \
+                or field_array[new_apple_x][new_apple_y+1] != 0 \
+                    or field_array[new_apple_x-1][new_apple_y] != 0 \
+                        or field_array[new_apple_x-1][new_apple_y-1] != 0 \
+                            or field_array[new_apple_x-1][new_apple_y+1] != 0 \
+                                or field_array[new_apple_x+1][new_apple_y] != 0 \
+                                    or field_array[new_apple_x+1][new_apple_y-1] != 0 \
+                                        or field_array[new_apple_x+1][new_apple_y+1] != 0):
+            new_apple_x = randint(0, 14)
+            new_apple_y = randint(0, 8)
+        appl = apple(new_apple_x, new_apple_y)
 
 def get_distance(x1, y1, x2, y2):
     return sqrt((x1-x2)**2 + (y1-y2)**2)
@@ -133,6 +154,7 @@ def check_collide():
 
 while(acting):
     clear_canvas()
+    field_array_reset()
     img_field.draw(UI_WIDTH // 2, UI_HEIGHT // 2)
     appl.draw()
     snake_move_and_draw()
