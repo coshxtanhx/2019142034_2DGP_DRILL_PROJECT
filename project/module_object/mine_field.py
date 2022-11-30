b5075bacc2b6b0784eb18158c1fc57c216a4bcc9
Cho_YongJoon <yzz1108j@gmail.com>
2022-11-30 23:27:22 +0900

    mine field module updated
    2019142034

--- project/module_object/mine_field.py
+++ project/module_object/mine_field.py
@@ -1,4 +1,6 @@
 from module_other.coordinates_module import *
+from module_other.term_table import *
+import module_other.game_framework as gf
 
 class Mine_field:
     def __init__(self, parent, dx, dy):
@@ -6,3 +8,10 @@ class Mine_field:
         self.gx = parent.gx + dx
         self.gy = parent.gy + dy
         self.x, self.y = grid_to_coordinates(self.gx, self.gy)
+    def update(self):
+        pass
+    def draw(self):
+        pass
+    def handle_collision(self, other, group):
+        if group == COL_PHEAD_MINEFIELD:
+            self.parent.counter -= gf.elapsed_time
