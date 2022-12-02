19f3849a090e0baddf5630b915d84027f051723e
Cho_YongJoon <yzz1108j@gmail.com>
2022-12-02 20:24:08 +0900

    pico2d lib directory added
    2019142034

--- project/module_other/save_file_manager.py
+++ project/module_other/save_file_manager.py
@@ -1,4 +1,8 @@
 import pickle
+from module_other.term_table import *
 
 class SaveFile:
-    pass
+    def __init__(self):
+        self.cur_stage = STAGE1
+        self.cur_character = GENERAL_SNAKE
+        self.stars = 0
