4003ac4336ebff0e29a8025fa5fcc0d6570d45cd
Cho_YongJoon <yzz1108j@gmail.com>
2022-12-03 15:43:36 +0900

    cheat key manager updated
    2019142034

--- project/module_other/cheat_key_manager.py
+++ project/module_other/cheat_key_manager.py
@@ -1,7 +1,9 @@
 import json
 
 cheat_enabled = None
-
-with open('data/cheat.json', 'r') as f:
-    data = json.load(f)
-    cheat_enabled = data["press_P_key_to_cheat"]
+try:
+    with open('data/cheat.json', 'r') as f:
+        data = json.load(f)
+        cheat_enabled = data["press_P_key_to_cheat"]
+except:
+    cheat_enabled = False
